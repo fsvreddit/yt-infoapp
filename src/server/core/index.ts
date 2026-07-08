f1ecb4eb856c97e38a502c0d86f2aa43e86c9bb7
kathrynweston <266851988+CR29-22-2805@users.noreply.github.com>
2026-07-08 13:28:21 -0500

    Add optional video description link redaction

--- src/server/core/index.ts
+++ src/server/core/index.ts
@@ -2,5 +2,6 @@ export * from "./actionContentBasedOnThresholds.js";
 export * from "./actionExemptionChecks.js";
 export * from "./addInfoComment.js";
 export * from "./appSettings.js";
+export * from "./descriptionFormatting.js";
 export * from "./helpers.js";
 export * from "./youtubeData.js";
